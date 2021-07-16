2a7bd9cdcfd214eb0064da7e781965a65edad196
Andrew Franklin <andrewfranklin@Andrews-MacBook-Pro.local>
2021-07-16 16:37:15 -0700

    bug fix

--- src/routes/dashboard.ts
+++ src/routes/dashboard.ts
@@ -5,7 +5,7 @@ export default (req: Request, res: Response) => {
   const ai: any = authInfo(req as UserRequest);
 
   res.render('dashboard', {
-    session: ai.claims,
+    session: ai.claims.session,
     url: req.hostname
   });
 };
