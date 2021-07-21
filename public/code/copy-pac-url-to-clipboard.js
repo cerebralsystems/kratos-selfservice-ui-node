38d245c689085899e3bc7627cc28e9846873cd43
Andrew Franklin <andrewfranklin@Andrews-MacBook-Pro.local>
2021-07-20 17:01:07 -0700

    style update

--- public/code/copy-pac-url-to-clipboard.js
+++ public/code/copy-pac-url-to-clipboard.js
@@ -1,7 +1,17 @@
-var copyButton = document.getElementById('copy-button');
+function copy () {
+  var copyText = document.getElementById('copy');
+  if (navigator.clipboard) {
+    navigator.clipboard.writeText(event.target.value);
+  } else {
+    copyText.select();
+    copyText.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+  }
 
-copyButton.addEventListener('click', function (event) {
-  console.log(event.target.value);
-  navigator.clipboard.writeText(event.target.value);
-  event.target.text = 'Copied!';
-});
+  const confirmation = document.getElementById('copied');
+
+  confirmation.style.display = 'block';
+  setTimeout(function () {
+    confirmation.style.display = 'none';
+  }, 2000);
+}
